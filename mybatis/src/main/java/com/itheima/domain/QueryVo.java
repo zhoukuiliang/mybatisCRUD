9fb7c2ed63a334649b017316ad5796fed20abf7f
zhoukuiliang <873169501@qq.com>
2021-05-14 09:38:29 +0800

    mybatisCRUD
    mybatisCRUD  mybatis的基本增删改查操作

---
+++ mybatis/src/main/java/com/itheima/domain/QueryVo.java
@@ -0,0 +1,18 @@
+package com.itheima.domain;
+
+/**
+ * @author
+ * @Company
+ */
+public class QueryVo {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
